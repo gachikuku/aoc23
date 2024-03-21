d1562c826b21df42d37f4f4d1e0e94e49aa94307
Alnoman Kamil <noman@kamil.gr>
2024-03-21 20:12:02 +0200

    generative ai got me the first star

--- day01/main.go
+++ day01/main.go
@@ -3,23 +3,51 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("test.txt")
+
+	file, err := os.Open("input.txt")
 	if err != nil {
-		log.Fatalf("failed to open file: %s", err)
+		fmt.Println("Error opening file:", err)
+		return
 	}
+	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
+	sum := 0
 
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		line := scanner.Text()
+		number := extractNumber(line)
+		sum += number
 	}
 
-	file.Close()
+	fmt.Println("The sum of all calibration values is:", sum)
+}
+
+func extractNumber(line string) int {
+	firstDigit := -1
+	lastDigit := -1
+
+	for _, char := range line {
+		if char >= '0' && char <= '9' {
+			if firstDigit == -1 {
+				firstDigit = int(char - '0')
+			}
+			lastDigit = int(char - '0')
+		}
+	}
+
+	if firstDigit == -1 || lastDigit == -1 {
+		return 0
+	}
+
+	if firstDigit == lastDigit {
+		return 11 * firstDigit
+	}
+
+	return 10*firstDigit + lastDigit
 }
 
