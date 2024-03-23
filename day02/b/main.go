bc25337254975c142375ed01934552a616b12363
Alnoman Kamil <noman@kamil.gr>
2024-03-23 05:40:04 +0200

    let it aid you not replace you

---
+++ day02/b/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+    "strconv"
+)
+
+func gameID(conudrum string) int {
+    gameRegex := regexp.MustCompile(`Game (\d+)`)
+    gameMatch := gameRegex.FindStringSubmatch(conudrum)
+    gameNumber := gameMatch[1]
+    result, err := strconv.Atoi(gameNumber)
+
+    if err != nil {
+        panic(err)
+    } 
+
+    return result
+}
+
+func minima(conudrum string) int {
+    var red_max int
+    var green_max int
+    var blue_max int
+    colorRegex := regexp.MustCompile(`(\d+) (\w+)`)
+    colorMatches := colorRegex.FindAllStringSubmatch(conudrum, -1)
+    for _, match := range colorMatches {
+        colorNumber := match[1]
+        number, err := strconv.Atoi(colorNumber)
+        if err != nil {
+            panic(err)
+        }
+        colorName := match[2]
+        switch colorName {
+        case "red":
+            if number > red_max { red_max = number }
+        case "green":
+            if number > green_max { green_max = number }
+        case "blue":
+            if number > blue_max { blue_max = number }
+        }
+    }
+
+    return red_max * green_max * blue_max
+}
+
+
+func main() {
+
+    var ans int
+
+    readFile, err := os.Open("input.txt")
+
+    if err != nil {
+        panic(err)
+    }
+
+    fileScanner := bufio.NewScanner(readFile)
+
+    fileScanner.Split(bufio.ScanLines)
+
+    for fileScanner.Scan() {
+            ans += minima(fileScanner.Text())
+        }
+
+    readFile.Close()
+    fmt.Println(ans)
+}
+
+
+
